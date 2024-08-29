00859ba9a8e31608236ca58ba104b708a125d599
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-08-29 11:12:20 -0300

    feat add initial migration

---
+++ EntiyFrameworkCore/Migrations/20240829140349_init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntiyFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "Varchar(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "Varchar(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "Varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
