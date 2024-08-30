634611f1716fc6c02f85be904378013c3fdf5c6f
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-08-30 10:31:49 -0300

    feat: aplay configuration and dependece injection

--- JWT/Program.cs
+++ JWT/Program.cs
@@ -1,6 +1,15 @@
+using JWT.Models;
+using JWT.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<TokenService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (TokenService service)
+=> service.GenerateToken(user: new User(Guid.NewGuid(), "ERICKKRAMER@HOTMAIL.COM", "ERICKKRAMER123@", [
+    "student", "premium"
+])));
 
 app.Run();
